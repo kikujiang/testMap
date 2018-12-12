9dc8b01f58fcfa352a210ae680e20f94556b2c63
kikujiang <powchina2012@gmail.com>
2018-12-12 23:28:59 +0800

    添加坐标显示功能

--- app/src/main/java/map/test/testmap/Constants.java
+++ app/src/main/java/map/test/testmap/Constants.java
@@ -2,51 +2,52 @@ package map.test.testmap;
 
 public class Constants {
 
-    public static final String TEST_URL = "http://172.19.26.52:8080/em";
-    public static final String URL = "http://119.3.71.206";
+//    public static final String TEST_URL = "http://172.19.26.52:8080/em";
+    public static final String TEST_URL = "http://119.3.71.206";
+    public static final String SPLITTER = "/";
 
     /**
      * 登陆
      */
-    public static final String TAG_LOGIN = "/app/loginClient";
+    public static final String TAG_LOGIN = SPLITTER + "loginClient";
     /**
      * 获取标记点类型列表
      */
-    public static final String TAG_GET_POINT_TYPE = "/tagAction/app/getTagTypeList";
+    public static final String TAG_GET_POINT_TYPE = "/tagAction"+SPLITTER+"getTagTypeList";
     /**
      * 获取线路类型列表
      */
-    public static final String TAG_GET_LINE_TYPE = "/tagAction/app/getTagLineTypeList";
+    public static final String TAG_GET_LINE_TYPE = "/tagAction"+SPLITTER+"getTagLineTypeList";
     /**
      * 获取所有标记点
      */
-    public static final String TAG_GET_ALL_POINT = "/tagAction/app/getAllTag";
+    public static final String TAG_GET_ALL_POINT = "/tagAction"+SPLITTER+"getAllTag";
     /**
      * 获取某个标记点
      */
-    public static final String TAG_GET_SINGLE_POINT = "/tagAction/app/getTag";
+    public static final String TAG_GET_SINGLE_POINT = "/tagAction"+SPLITTER+"getTag";
     /**
      *保存某个标记点
      */
-    public static final String TAG_SAVE_SINGLE_POINT = "/tagAction/app/saveTag";
+    public static final String TAG_SAVE_SINGLE_POINT = "/tagAction"+SPLITTER+"saveTag";
     /**
      * 删除某个标记点
      */
-    public static final String TAG_DELETE_SINGLE_POINT = "/tagAction/app/delTag";
+    public static final String TAG_DELETE_SINGLE_POINT = "/tagAction"+SPLITTER+"delTag";
     /**
      * 获取所有线路
      */
-    public static final String TAG_GET_ALL_LINE = "/tagAction/app/getAllTagLine";
+    public static final String TAG_GET_ALL_LINE = "/tagAction"+SPLITTER+"getAllTagLine";
     /**
      * 获取某条线路
      */
-    public static final String TAG_GET_SINGLE_LINE = "/tagAction/app/getTagLine";
+    public static final String TAG_GET_SINGLE_LINE = "/tagAction"+SPLITTER+"getTagLine";
     /**
      * 保存某条线路
      */
-    public static final String TAG_SAVE_SINGLE_LINE = "/tagAction/app/saveTagLine";
+    public static final String TAG_SAVE_SINGLE_LINE = "/tagAction"+SPLITTER+"saveTagLine";
     /**
      * 删除某条线路
      */
-    public static final String TAG_DELETE_SINGLE_LINE = "/tagAction/app/delTagLine";
+    public static final String TAG_DELETE_SINGLE_LINE = "/tagAction"+SPLITTER+"delTagLine";
 }
--- app/src/main/java/map/test/testmap/MainActivity.java
+++ app/src/main/java/map/test/testmap/MainActivity.java
@@ -511,7 +511,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);
 //        myLocationStyle.anchor((float)currentLocation.getLatitude(),(float)currentLocation.getLongitude());
-        myLocationStyle.anchor(0.0f,1.0f);
+//        myLocationStyle.anchor(0.0f,0.5f);
         myLocationStyle.showMyLocation(true);
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
