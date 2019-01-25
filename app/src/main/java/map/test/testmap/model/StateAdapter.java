686c8e2e3f4105d750b93054497eda66c7481510
kikujiang <529119401>
2019-01-25 18:01:10 +0800

    2019.01.25dev版本提交，年前更新版本

---
+++ app/src/main/java/map/test/testmap/model/StateAdapter.java
@@ -0,0 +1,62 @@
+package map.test.testmap.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import map.test.testmap.R;
+
+public class StateAdapter extends BaseAdapter {
+
+    ArrayList<State> stateList;
+    private LayoutInflater mInflater;
+
+    public StateAdapter(Context context,ArrayList<State> list){
+        mInflater = LayoutInflater.from(context);
+        this.stateList = list;
+    }
+
+    @Override
+    public int getCount() {
+        return stateList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return stateList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+             if (convertView == null) {
+                    convertView = mInflater.inflate(R.layout.layout, parent, false); //加载布局
+                    holder = new ViewHolder();
+
+                    holder.text = (TextView) convertView.findViewById(R.id.text);
+
+                   convertView.setTag(holder);
+             } else {   //else里面说明，convertView已经被复用了，说明convertView中已经设置过tag了，即holder
+                   holder = (ViewHolder) convertView.getTag();
+             }
+
+             State current = stateList.get(position);
+             holder.text.setText(current.getCreateTime()+" "+current.getCreateUserName()+" "+current.getName());
+
+             return convertView;
+    }
+
+    private class ViewHolder {
+         TextView text;
+    }
+}
