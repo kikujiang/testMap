6f3f4183ad80e02f93c104bdc6c3aa38785f2dee
kikujiang <529119401>
2019-12-25 17:59:53 +0800

    2019.12.16版本提交解决了4.4以下崩溃的问题

--- app/src/main/java/map/test/testmap/MapApplication.java
+++ app/src/main/java/map/test/testmap/MapApplication.java
@@ -1,9 +1,15 @@
 package map.test.testmap;
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.multidex.MultiDex;
+
 import com.tencent.bugly.crashreport.CrashReport;
 
 import org.litepal.LitePalApplication;
 
-public class MapApplication extends LitePalApplication {
+public class MapApplication extends LitePalApplication implements Application.ActivityLifecycleCallbacks{
 
     @Override
     public void onCreate() {
@@ -12,4 +18,44 @@ public class MapApplication extends LitePalApplication {
         CrashReport.initCrashReport(getApplicationContext(), "171b14d1df", false);
     }
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
 }
